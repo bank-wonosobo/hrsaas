2e5acf13c46c3cb5df5965cc83a7da4390f01851
yokunoshita <hanafiulinnuha1812@gmail.com>
2026-08-24 15:06:31 +0700

    feat: add get all time off types on admin api

--- apps/api/internal/modules/time_off/delivery/http/admin/time_off_type_router.go
+++ apps/api/internal/modules/time_off/delivery/http/admin/time_off_type_router.go
@@ -11,6 +11,7 @@ func (c *TimeOffTypeController) RegisterRoutes(
 	protected middleware.ProtectedMiddleware,
 ) {
 	route := router.Group("/time-off-types")
+	route.Get("", protected("TIME_OFF_TYPES", c.ListTypes)...)
 	route.Post("/", protected("TIME_OFF_TYPES", c.CreateType)...)
 	route.Put("/:id", protected("TIME_OFF_TYPES", c.UpdateType)...)
 	route.Delete("/:id", protected("TIME_OFF_TYPES", c.DeleteType)...)
